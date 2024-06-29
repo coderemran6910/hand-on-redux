6d18eca1b1551fbc5916827b2967ee336015da02
Coder Emran <coderemran6910@gmail.com>
2024-06-30 05:25:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import { Button } from "./components/ui/button";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline"> Todo Application </h1>
+      <Button>Button</Button>
+    </>
+  );
+}
+
+export default App;
